18d7f96a6a069139a71386fd5be995e39d0e5edc
corite <me@corite.dev>
2023-04-27 22:29:59 +0200

    fix: remove unused dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,12 +10,10 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven { url= uri("https://jitpack.io") }
 }
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("com.github.corite:aes-128:v0.5-alpha")
 }
 
 tasks.test {
